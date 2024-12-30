d79d7edf0292d71d9ff228c439e4047b3dba9a62
Pratham Kukudkar <87323200+PRATHAMPRAJU@users.noreply.github.com>
2024-12-30 15:42:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 terraform {
-    required_version = ">= 1.0.0"  # Specify the required version
+required_version = ">= 1.0.0"  # Specify the required version
 }
 
 resource "aws_vpc" "example_vpc" {
-  cidr_block       = var.cidr_block
-  instance_tenancy = var.instance_tenancy
+cidr_block       = var.cidr_block
+instance_tenancy = var.instance_tenancy
 
-  tags = {
-    Name = var.vpc_name
-  }
+tags = {
+Name = var.vpc_name
 }
+}
+
