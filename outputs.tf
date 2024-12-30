5329adaba8d3487d033301f0b8b8574198973b7d
Pratham Kukudkar <87323200+PRATHAMPRAJU@users.noreply.github.com>
2024-12-30 20:23:49 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "vpc_id" {
-  description = "The ID of the VPC."
-  value       = aws_vpc.example_vpc.id
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.example.id
+}
+
+output "public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.example.public_ip
 }
